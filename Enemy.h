9890bcfd96532facd5dae46bd09f844069568c6c
Ayush-20034 <151995196+Ayush-20034@users.noreply.github.com>
2023-11-25 13:58:55 +0530

    Create Enemy.h

---
+++ Enemy.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef ENEMY_H
+#define ENEMY_H
+#include<SFML/Graphics.hpp>
+class Enemy
+
+{
+private:
+	unsigned pointCount;
+	sf::CircleShape shape;
+	int type;
+	float speed;
+	int hp;
+	int hpMax;
+	int damage;
+	int points;
+
+
+
+
+
+	void initVariables();
+	void initShape();
+
+public:
+	Enemy(float pos_x, float pos_y);
+	virtual ~Enemy();
+	//Accessor
+	const sf::FloatRect getBounds() const;
+	const int& getPoints() const;
+	const int& getDamage() const;
+
+	//functions
+	void update();
+	void render(sf::RenderTarget*target);
+};
+
+
+
+#endif //!ENEMY_H
